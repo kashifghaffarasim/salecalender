51584a5b0e5e8f8dacbafad584d9a15d5b818f55
Kashif Ghaffar Asim <kashif.ghaffar100@gmail.com>
2018-08-08 12:08:38 +0500

    selection plans

--- src/App.js
+++ src/App.js
@@ -1,30 +1,23 @@
 import React, { Component } from 'react';
 import Calender from './components/calender/calender';
 import Element from './components/products/showmore';
+import Header from './components/commen/header';
 
 class App extends Component {
       constructor(props) {
           super(props);
-
       }
 
   render() {
     return (
-      <div className="App">
-          <div className="row">
-             <div className="col-md-12 text-center">
-                <Element />
-            </div>
-          </div>
-         <div className="row">
-            <div className="col-md-2" ></div>
-             <div className="col-md-8" >
-                  <Calender />
-              </div>
-                <div className="col-md-2" ></div>
-          </div>
 
-      </div>
+      <main role="main" className="App">
+            <div className="container-fluid">
+               <Header />
+               <Calender />
+            </div>
+      </main>
+
     );
   }
 }
--- src/components/commen/header.js
+++ src/components/commen/header.js
@@ -6,29 +6,58 @@ class Header extends Component {
 
     return (
 
-
-      <header className="header">
-          <nav className="navbar navbar-default navbar-static-top">
-                <div className="container-fluid">
-                    <div class="navbar-header">
-                          <button type="button" class="navbar-toggle navbar-toggle-sidebar collapsed">
-                          MENU
-                          </button>
-                          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                            <span class="sr-only">Toggle navigation</span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                          </button>
-                          <a class="navbar-brand" href="#">
-                            Administrator
-                          </a>
-                    </div>
-                </div>
-          </nav>
-      </header>
+          <div className="row">
+              <div className="col-md-2 p-0">
+                  <div className="sidebar text-center">
+                         <div className="navbar-brand">
+                           Dashboard
+                         </div>
+                         <ul className="list-unstyled">
+                               <li className="sidebar-items active"><a href=""><i className="fa fa-dashboard mr-3 fa-2x"></i>Dashboard</a></li>
+                               <li className="sidebar-items"><a href=""><i className="fa fa-umbrella mr-3 fa-2x"></i>Products</a></li>
+                               <li className="sidebar-items"><a href=""><i className="fa fa-calendar mr-3 fa-2x"></i>Sales Calender</a></li>
+                               <li className="sidebar-items"><a href=""><i className="fa fa-signal mr-3 fa-2x"></i>Income</a></li>
+                          </ul>
+                   </div>
+               </div>
 
 
+               <div className="col-md-10">
+          						<div className="navbar">
+                  						       	<ul className="list-unstyled nav">
+                                								<li className="nav-link custom-nav">
+                                                      <a>
+                                                            <i className="fa fa-bell"></i><span className="counter">12</span>
+                                      								</a>
+                                                </li>
+                                								<li className="nav-link custom-nav">
+                                                       <a>
+                                                            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 18 18"><path d="M4.5 11H3v4h4v-1.5H4.5V11zM3 7h1.5V4.5H7V3H3v4zm10.5 6.5H11V15h4v-4h-1.5v2.5zM11 3v1.5h2.5V7H15V3h-4z"/></svg>
+                                                       </a>
+                                                  </li>
+                  						        	</ul>
+          						                	<ul className="list-unstyled nav">
+                                    								<li className="nav-link custom-nav">
+                                                          <span className="text-muted">Hello,</span>
+                                                          <span className="font-weight-bold">User</span>
+                                                    </li>
+                                    								<li className="nav-item avatar dropdown custom-nav">
+                                    									<a className="nav-link dropdown-toggle waves-effect waves-light custom-link" id="navbarDropdownMenuLink-5" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                                    										<img src="https://mdbootstrap.com/img/Photos/Avatars/avatar-2.jpg" className="rounded-circle z-depth-0" alt="avatar image" />
+                                    									</a>
+                                    									<div className="dropdown-menu dropdown-menu-right dropdown-secondary" aria-labelledby="navbarDropdownMenuLink-5">
+                                    										<a className="dropdown-item waves-effect waves-light" href="#">Action</a>
+                                    										<a className="dropdown-item waves-effect waves-light" href="#">Another action</a>
+                                    										<a className="dropdown-item waves-effect waves-light" href="#">Something else here</a>
+                                    									</div>
+                                    								</li>
+          					                   		</ul>
+          						</div>
+                	</div>
+
+
+
+          </div>
 
     );
   }
